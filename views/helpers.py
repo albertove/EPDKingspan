5a6deb8902f6e279ca6ab10402a20a50c2c25cd4
albertove <alvega27@gmail.com>
2025-05-24 13:27:03 +0200

    Delete views/helpers.py

--- views/helpers.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def load_css(path="assets/style.css"):
-    with open(path) as f:
-        css = f"<style>{f.read()}</style>"
-        st.markdown(css, unsafe_allow_html=True)
